a3b6d8f55e01a654a9afe1728c932a00c9715f3a
Diego Valenzuela Duque <diegovalenz@uchile.cl>
2023-01-16 22:13:07 -0300

    ruta adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -259,7 +259,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => '/',
+    'dashboard_url' => '/home',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
