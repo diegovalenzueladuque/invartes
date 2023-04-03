bbf183a24a560d49d6839e202b69cde120a4e116
Diego Valenzuela Duque <diegovalenz@uchile.cl>
2023-04-02 21:38:45 -0400

    mejoras pdf

--- resources/views/computadores/pdf.blade.php
+++ resources/views/computadores/pdf.blade.php
@@ -11,15 +11,16 @@
     <title>Document</title>
 </head>
 <body>
-    <div><h1 style="text-align:center; color:#08315e; font-size: 2rem; border:solid black 1px; border-radius: 6px;">REPORTE COMPUTADORES</h1></div><br><br>
+    <div><img src="https://escudouchile.files.wordpress.com/2012/05/escudo-universidad-de-chile-lineal-2.png" style="width: 40%; margin-top: -60px; margin-bottom: 200px;" align="right" alt=""></div><br>
+    <div style="margin-bottom: 120px; margin-top: 60px;"><h1 style="text-align:center; color:#08315e; font-size: 2rem; border:solid black 1px; border-radius: 12px; background-color:#b3b3b4; font-weight: bold; width:45%">REPORTE COMPUTADORES</h1></div><br>
     <table class="table table-hover">
         <tr style="border-radius: 8px">
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">ETIQUETA</th>
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">MARCA</th>
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">CPU</th>
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">MEMORIA</th>            
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">SIST. OPERATIVO</th>
-            <th style="text-align:center; color:#08315e; font-size: 1,5rem; border:solid black 1px; ">UNIDAD</th>
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">ETIQUETA</th>
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">MARCA</th>
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">CPU</th>
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">MEMORIA</th>            
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">SIST. OPERATIVO</th>
+            <th style="text-align:center; color:#000; font-size: 1,5rem; border:solid black 1px; background-color:#b3b3b4; font-weight: bold; ">UNIDAD</th>
 
         </tr>
         @foreach ($computadores as $computador )
